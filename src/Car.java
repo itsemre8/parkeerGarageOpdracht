322aa3c8a2abc09e0f708a5766eba78c93cf4698
emre <1211471@student.roc-nijmegen.nl>
2026-06-19 10:26:36 +0200

    car klasse aangemaakt

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    private String licensePlate;
+    private String brand;
+    private String model;
+    private String colour;
+
+    public Car(String licensePlate, String brand, String model, String colour) {
+        this.licensePlate = licensePlate;
+        this.brand = brand;
+        this.model = model;
+        this. colour = colour;
+    }
+}
