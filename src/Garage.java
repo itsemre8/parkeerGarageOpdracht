31ae9ff8322c3932b85a486f52d4319c4ccdcc68
emre <1211471@student.roc-nijmegen.nl>
2026-06-20 14:03:39 +0200

    Garage klasse gemaakt

--- src/Garage.java
+++ src/Garage.java
@@ -1,3 +1,24 @@
-public class Garage
-{
-}
+import java.util.ArrayList;
+
+public class Garage {
+
+    private static int counter = 1;
+    private ArrayList<Car> parkedCars;
+    private int id;
+    private int maxCapacity;
+
+    public boolean checkIfValid(Car car, License license) {
+        if (parkedCars.size() >= maxCapacity) {
+            return false;
+        }
+
+        if (!license.getLicenseHolderPlate().equals(car.getLicensePlate())) {
+            return false;
+        }
+
+        if (license.getValidInGarageId() != id) {
+            return false;
+        }
+        return true;
+    }
+} 
