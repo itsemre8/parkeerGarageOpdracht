ac6ef2a2c2b0f556004df265dae4d742c96536e2
emre <1211471@student.roc-nijmegen.nl>
2026-06-20 13:16:16 +0200

    license klass afgemaakt

--- src/License.java
+++ src/License.java
@@ -1,3 +1,21 @@
-public class License
-{
+public class License {
+    private static int counter = 1;
+    private Car licenseHolder;
+    private int id;
+    private int validGarageId;
+
+    public License(Car licenseHolder, int validInGarageId) {
+        this.licenseHolder = licenseHolder;
+        this.validGarageId = validInGarageId;
+        this.id = counter;
+        counter++;
+    }
+
+    public String getLicenseHolderPlate() {
+        return licenseHolder.getLicensePlate();
+    }
+
+    public int getValidInGarageId() {
+        return validGarageId;
+    }
 }
